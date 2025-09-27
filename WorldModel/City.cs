9b50e7c7166a0d660cbe23a465eebd0796f89377
Erin <erinjmaldonado@gmail.com>
2025-09-26 18:09:14 -0700

    c# to create DB

---
+++ WorldModel/City.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("country_id")]
+    public int CountryId { get; set; }
+
+    [Column("name")]
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column("latitude", TypeName = "decimal(18, 0)")]
+    public decimal Latitude { get; set; }
+
+    [Column("longitude", TypeName = "decimal(18, 0)")]
+    public decimal Longitude { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
