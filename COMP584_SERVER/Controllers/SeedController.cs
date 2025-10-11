1a300c803509024891ce910d1c553b7e4f301b75
Erin <erinjmaldonado@gmail.com>
2025-10-10 17:53:29 -0700

    Add CSV handling and database population features
    Updated `COMP584_SERVER.csproj` to include new package references for `CsvHelper`, `Microsoft.AspNetCore.OpenApi`, and `Microsoft.EntityFrameworkCore`.
    
    Modified `SeedController` to read from a CSV file and populate the database with country and city records, including a new parameter for `IHostEnvironment`.
    
    Introduced `Comp584CSV` class to define the structure of the data being read from the CSV file, enhancing data management capabilities.

--- COMP584_SERVER/Controllers/SeedController.cs
+++ COMP584_SERVER/Controllers/SeedController.cs
@@ -1,16 +1,50 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using COMP584_SERVER.Data;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Globalization;
 using WorldModel;
 
 namespace COMP584_SERVER.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SeedController(Comp584DbContext context) : ControllerBase
+    public class SeedController(Comp584DbContext context, IHostEnvironment environment) : ControllerBase
     {
+        string _pathName = Path.Combine(environment.ContentRootPath, "Data/worldcities.csv");
+
         [HttpPost("Countries")]
         public async Task<ActionResult> PostCountries()
         {
+            Dictionary<string, Country> countries = await context.Countries.AsNoTracking().
+                ToDictionaryAsync(c => c.Name, StringComparer.OrdinalIgnoreCase);
+            CsvConfiguration config = new(CultureInfo.InvariantCulture) { 
+                HasHeaderRecord = true, 
+                HeaderValidated = null 
+            }; 
+
+            using StreamReader reader = new(_pathName); 
+            using CsvReader csv = new(reader, config); 
+            List<Comp584CSV> records = csv.GetRecords<Comp584CSV>().ToList();
+
+            foreach (Comp584CSV record in records)
+            {
+                if (!countries.ContainsKey(record.country))
+                {
+                    Country country = new() {
+                        Name = record.country,
+                        Iso2 = record.iso2,
+                        Iso3 = record.iso3
+                    };
+
+                    countries.Add(country.Name, country);
+                    await context.Countries.AddAsync(country);
+                }
+            }
+
             await context.SaveChangesAsync();
 
             return Ok();
@@ -19,9 +53,38 @@ namespace COMP584_SERVER.Controllers
         [HttpPost("Cities")]
         public async Task<ActionResult> PostCities()
         {
+            Dictionary<string, Country> countries = await context.Countries.AsNoTracking().
+                    ToDictionaryAsync(c => c.Name, StringComparer.OrdinalIgnoreCase);
+            CsvConfiguration config = new(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = true,
+                HeaderValidated = null
+             };
+
+            using StreamReader reader = new(_pathName);
+            using CsvReader csv = new(reader, config);
+            List<Comp584CSV> records = csv.GetRecords<Comp584CSV>().ToList();
+            int cityCount = 0;
+            foreach (Comp584CSV record in records)
+            {
+                if (record.population.HasValue && record.population.Value > 0)
+                {
+                    City city = new()
+                    {
+                        Name = record.city,
+                        Latitude = record.lat,
+                        Longitude = record.lng,
+                        Population = (int)record.population.Value,
+                        CountryId = countries[record.country].Id
+                    };
+                    cityCount++;
+                    await context.Cities.AddAsync(city);
+                }
+            }
+
             await context.SaveChangesAsync();
 
-            return Ok();
+            return new JsonResult(cityCount);
         }
     }
 
---
+++ COMP584_SERVER/Data/Comp584CSV.cs
@@ -0,0 +1,17 @@
+﻿namespace COMP584_SERVER.Data
+{
+    public class Comp584CSV
+    {
+        public required string city { get; set; }
+        public required string city_ascii { get; set; }
+        public decimal lat { get; set; }
+        public required decimal lng { get; set; }
+        public required string country { get; set; }
+        public required string iso2 { get; set; }
+        public required string iso3 { get; set; }
+        public required string admin_name { get; set; }
+        public required string capital { get; set; }
+        public decimal? population { get; set; }
+        public long id { get; set; }
+    }
+}
